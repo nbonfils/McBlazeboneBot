890b4672427b6a1194fff0bedd3e4df8edf9e9eb
nbonfils <nils.bonfils@epfl.ch>
2017-03-05 20:53:47 +0100

    Add op list for bot

--- blazebonemcbot.lua
+++ blazebonemcbot.lua
@@ -5,9 +5,23 @@ local token = tokenFile:read("*all")
 -- import the bot framework
 local bot, extension = (require "modules.lua-bot-api").configure(token)
 
+-- define Set data type
+function Set (list)
+    local set = {}
+    for _, l in ipairs(list) do
+        set[l] = true
+    end
+    return set
+end
+
+-- op clearance :)
+local op = Set { "Nils", "Yann" }
+
 -- handle text message/commands
 extension.onTextReceive = function (msg)
     print("new message by", msg.from.first_name, ":", msg.text)
+
+
 end
 
 extension.run()

622d70025a08a41448e78533219404c37dbb8a77
nbonfils <nils.bonfils@epfl.ch>
2017-03-06 17:44:56 +0100

    Begin skeleton of the bot

--- blazebonemcbot.lua
+++ blazebonemcbot.lua
@@ -1,3 +1,6 @@
+
+-- VARIABLES DEFINITIONS
+
 -- retrive the Telegram bot token, so it's not hardcoded
 local tokenFile = io.open("token", "r")
 local token = tokenFile:read("*all")
@@ -17,11 +20,26 @@ end
 -- op clearance :)
 local op = Set { "Nils", "Yann" }
 
+
+-- MAIN PROGRAMME
+
 -- handle text message/commands
 extension.onTextReceive = function (msg)
     print("new message by", msg.from.first_name, ":", msg.text)
 
+    -- list online players
+    if msg.text == "/list" then
+        -- TODO
+    end
+
+    -- op commands
+    if op[msg.from.first_name] then
+        -- TODO
+    end
 
 end
+
+-- read server log and notify telegram
+-- TODO
 
 extension.run()
